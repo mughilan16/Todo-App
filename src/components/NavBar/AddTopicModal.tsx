30eb3b3466b82d50c35bc31afa8b8ff87272a9e8
mughilan16 <mughilan16052005@gmail.com>
2023-01-31 21:20:27 +0530

    Modal Design

--- src/components/NavBar/AddTopicModal.tsx
+++ src/components/NavBar/AddTopicModal.tsx
@@ -5,6 +5,9 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
+  Flex,
+  Input,
+  Button,
 } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
@@ -22,9 +25,52 @@ const AddTopicModal: React.FC = () => {
       <Modal isOpen={modal.open} onClose={() => handleClose()}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader textAlign="center" fontFamily="monospace">
+            Create a new group
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Modal Body</ModalBody>
+          <ModalBody>
+            <form>
+              <Flex
+                align="center"
+                direction="column"
+                mt={-3}
+                mb={4}
+                mr={4}
+                ml={2}
+              >
+                {/* <Text fontSize={18} mr={4}>
+                  Name:
+                </Text> */}
+                <Input
+                  height={10}
+                  textAlign="center"
+                  borderRadius={4}
+                  fontSize={18}
+                  fontFamily="monospace"
+                  placeholder="Group Name"
+                  _placeholder={{ color: "gray.800" }}
+                  _focus={{
+                    border: "1px solid blue.100",
+                  }}
+                  mb={2}
+                ></Input>
+                <Button
+                  bg="blue.500"
+                  width="100%"
+                  fontSize="18"
+                  fontFamily="monospace"
+                  fontWeight={500}
+                  color="whitesmoke"
+                  _hover={{
+                    bg: "blue.400",
+                  }}
+                >
+                  Create
+                </Button>
+              </Flex>
+            </form>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
